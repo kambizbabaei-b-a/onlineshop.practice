27281129bf8c461299717eceb2bf11c855dc2d2a
kambizbabaei <kambiz.a.babaei@gmail.com>
2021-03-29 02:35:03 +0430

    nothing

--- res/js/index.js
+++ res/js/index.js
@@ -8,7 +8,7 @@ var sTH = function (str) {
 const cat_1 = sTH('<div class="col-4 no-padding" id="cat-item-1"><img src="./res/img/laptops.jpg" alt="" class="fit-content" /></div>');
 const cat_2 = sTH('<div class="col-4 no-padding"id="cat-item-2"><img src="./res/img/home_bedding.jpg"alt=""class="fit-content"/></div>');
 const cat_3 = sTH('<div class="col-4 no-padding" id="cat-item-3" > <img src="./res/img/strip_lighting.jpg" alt="" class="fit-content"/></div>');
-const cat_4 = sTH('<div class="col-3"><img src="./res/img/groceries.jpg" alt="" class="fit-content" /></div>');
+const cat_4 = sTH('<div class="col-4 no-padding" id = "cat-item-3"><img src="./res/img/groceries.jpg" alt="" class="fit-content" /></div>');
 const cat_section = document.getElementById("category-section");
 cat_section.appendChild(cat_1);
 cat_section.appendChild(cat_2);
